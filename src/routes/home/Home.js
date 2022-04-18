456d3020a94725581e1c00aa265617a31eb2874d
ValEmpire <palma_arcival@yahoo.com>
2022-04-18 15:43:19 -0400

    finish home route

--- src/routes/home/Home.js
+++ src/routes/home/Home.js
@@ -3,9 +3,16 @@ import { Box, Typography } from "@mui/material";
 
 const Home = () => {
   return (
-    <Box>
-      <Typography variant="h4">Welcome to Weather App</Typography>
-      <Typography variant="h6">Please search for your city</Typography>
+    <Box
+      minHeight={"70vh"}
+      display="flex"
+      alignItems={"center"}
+      justifyContent="center"
+    >
+      <Box textAlign={"center"}>
+        <Typography variant="h4">Welcome to Weather App</Typography>
+        <Typography variant="h6">Please search for your city</Typography>
+      </Box>
     </Box>
   );
 };
