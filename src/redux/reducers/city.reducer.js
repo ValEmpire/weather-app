c3d2e588f006cc665e65037812d38b5ac7b7fe2e
ValEmpire <palma_arcival@yahoo.com>
2022-04-18 15:40:55 -0400

    create city reducer

---
+++ src/redux/reducers/city.reducer.js
@@ -0,0 +1,10 @@
+const initialState = {
+  selectedCity: null,
+};
+
+export const city = (state = initialState, action) => {
+  switch (action.type) {
+    default:
+      return { ...state };
+  }
+};
--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,6 +1,8 @@
 import { combineReducers } from "redux";
-
+import { city } from "./city.reducer";
 /**
  * This will combine all reducers that will be exported for redux store
  */
-export default combineReducers({});
+export default combineReducers({
+  city,
+});
