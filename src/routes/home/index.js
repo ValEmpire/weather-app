e972f177b204c1595c85230df662f5e6fd99ad4f
ValEmpire <palma_arcival@yahoo.com>
2022-04-19 10:50:48 -0400

    fetch weatherByCooredinates every 10 seconds

--- src/routes/home/index.js
+++ src/routes/home/index.js
@@ -1,15 +1,26 @@
-import React from "react";
+import React, { useCallback, useEffect } from "react";
 import { Grid } from "@mui/material";
 import Home from "./Home";
 import Weather from "./Weather";
 
 // Redux
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+import { getWeatherByCoordinates } from "../../redux/actions";
 
 const Homepage = (props) => {
   const weather = useSelector((state) => state.weather);
 
+  const dispatch = useDispatch();
+
   const { selectedCity } = useSelector((state) => state.city);
+
+  const handleWeather = useCallback(() => {
+    setInterval(() => dispatch(getWeatherByCoordinates(selectedCity)), 10000);
+  }, [dispatch, selectedCity]);
+
+  useEffect(() => {
+    handleWeather();
+  }, [handleWeather]);
 
   return (
     <Grid container justifyContent={"center"}>
